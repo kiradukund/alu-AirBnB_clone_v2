d8f67bda047c8b372fd832481782371a1b7640eb
kiradukund <k.iradukund@alustudent.com>
2026-09-16 02:11:05 -0700

    Fix nginx hbnb_static location block in setup script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,6 +18,8 @@ HTML
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
-sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+config="server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n}"
+echo -e "$config" > /etc/nginx/sites-available/default
 
 service nginx restart
+exit 0
