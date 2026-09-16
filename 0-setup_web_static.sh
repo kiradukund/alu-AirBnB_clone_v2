fc8f124f15ae42ee38a8268a27632315020f9b7c
kiradukund <k.iradukund@alustudent.com>
2026-09-16 06:35:49 -0700

    Fix nginx config using printf for hbnb_static location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,21 +5,18 @@ apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-cat > /data/web_static/releases/test/index.html << 'HTML'
-<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-HTML
+</html>" > /data/web_static/releases/test/index.html
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
-config="server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n}"
-echo -e "$config" > /etc/nginx/sites-available/default
+printf "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\tserver_name _;\n\troot /var/www/html;\n\tindex index.html index.htm;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n}\n" > /etc/nginx/sites-available/default
 
 service nginx restart
 exit 0
