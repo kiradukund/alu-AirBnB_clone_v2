00be28db10aaefa9c8d6e3951684f72de30efc7d
kiradukund <k.iradukund@alustudent.com>
2026-09-17 00:02:41 -0700

    Fix nginx config using heredoc for hbnb_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,7 +16,21 @@ echo "<html>
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
-printf "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\tserver_name _;\n\troot /var/www/html;\n\tindex index.html index.htm;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n}\n" > /etc/nginx/sites-available/default
+cat > /etc/nginx/sites-available/default << 'NGINX'
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    root /var/www/html;
+    index index.html index.htm;
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+    }
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+NGINX
 
 service nginx restart
 exit 0
