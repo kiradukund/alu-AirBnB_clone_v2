2baf1855669a9fc8783154ed18ba5d2ff95c8033
kiradukund <k.iradukund@alustudent.com>
2026-09-19 02:55:59 -0700

    Fix nginx setup with explicit sites-enabled symlink and test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Sets up web servers for deployment of web_static
+apt-get -y update
 apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
@@ -16,21 +17,20 @@ echo "<html>
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
-cat > /etc/nginx/sites-available/default << 'NGINX'
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name _;
-    root /var/www/html;
-    index index.html index.htm;
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-    }
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-NGINX
-
-service nginx restart
+nginx_conf="server {
+\tlisten 80 default_server;
+\tlisten [::]:80 default_server;
+\tserver_name _;
+\troot /var/www/html;
+\tindex index.html index.htm;
+\tlocation /hbnb_static/ {
+\t\talias /data/web_static/current/;
+\t}
+\tlocation / {
+\t\ttry_files \$uri \$uri/ =404;
+\t}
+}"
+echo -e "$nginx_conf" | sudo tee /etc/nginx/sites-available/default
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo nginx -t && sudo service nginx restart
 exit 0
