3bf6e0b929f2fe6729d9ea497ceab9fedd08a86f
kiradukund <k.iradukund@alustudent.com>
2026-09-20 22:48:55 -0700

    Fix nginx: stop then start instead of restart, add index directive

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,20 +17,23 @@ echo "<html>
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
-nginx_conf="server {
-\tlisten 80 default_server;
-\tlisten [::]:80 default_server;
-\tserver_name _;
-\troot /var/www/html;
-\tindex index.html index.htm;
-\tlocation /hbnb_static/ {
-\t\talias /data/web_static/current/;
-\t}
-\tlocation / {
-\t\ttry_files \$uri \$uri/ =404;
-\t}
-}"
-echo -e "$nginx_conf" | sudo tee /etc/nginx/sites-available/default
-sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sudo nginx -t && sudo service nginx restart
+cat > /etc/nginx/sites-available/default << 'NGINX'
+server {
+listen 80 default_server;
+listen [::]:80 default_server;
+server_name _;
+root /var/www/html;
+index index.html index.htm;
+location /hbnb_static/ {
+alias /data/web_static/current/;
+index index.html;
+}
+location / {
+try_files $uri $uri/ =404;
+}
+}
+NGINX
+
+ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+nginx -s stop 2>/dev/null; sleep 1; service nginx start
 exit 0
